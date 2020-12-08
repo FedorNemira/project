07eafc461871716b1842fc51eaaa95b5dbbf423a
Fedor Nemira <fedor.nemira@gmail.com>
2020-12-08 21:53:57 +0300

    nemira:fix

--- project/poll/views.py
+++ project/poll/views.py
@@ -1,24 +1,28 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound 
+from django.http import HttpResponse
 from poll.models import Poll
 from poll import serializers
 import pprint
+from rest_framework import permissions
 
 
 class PollView(APIView):
     def get(self, request):
-        polls = Poll.objects.all()
-        serializer = serializers.PollSerializer(polls, many=True)
-        return Response({"polls": serializer.data})
+        if request.user.is_authenticated:
+            print(request.user)
+            polls = Poll.objects.all()
+            serializer = serializers.PollSerializer(polls, many=True)
+            return Response({"polls": serializer.data})
+        return HttpResponse("Only for registred users!")
 
     def post(self, request):
-        pprint.pprint(request.data)
-        poll = request.data
-        serializer = serializers.PollSerializer(data=poll)
-        if serializer.is_valid(raise_exception=True):
-            poll = serializer.save()
-        return Response({"success": f"Poll {poll} created successfully"})
+            poll = request.data
+            serializer = serializers.PollSerializer(data=poll)
+            if serializer.is_valid(raise_exception=True):
+                poll = serializer.save()
+            return Response({"success": f"Poll {poll} created successfully"})
 
     def delete(self, request):
         try:
--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'poll',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -119,7 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
